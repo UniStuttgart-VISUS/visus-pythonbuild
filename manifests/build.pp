efca0c5ff7993ff9c77a6a16d12b0ecd03ce07d7
Polka Administrator <enterprise-admins@visus.uni-stuttgart.de>
2021-08-24 12:32:05 +0200

    Initial version.

---
+++ manifests/build.pp
@@ -0,0 +1,61 @@
+# @summary Builds and installs a single version of Python.
+#
+# @api private
+define pythonbuild::build(
+        String $source_dir,
+        #String $install_dir,
+        String $base_url,
+        String $version,
+        Optional[String] $override_url = undef,
+        String $configure_flags = '--enable-optimizations',
+        String $make_target = 'altinstall') {
+
+    # Resolve the actual remote location of the sources.
+    $url = if $override_url {
+        $override_url
+    } else {
+        "${base_url}/${version}/Python-${version}.tgz"
+    }
+
+    # Resolve the local source file and directory.
+    $src_file = "${source_dir}/${basename($url)}"
+    $src_dir = "${source_dir}/${basename($url, '.*')}"
+
+    # Download and extract the sources.
+    archive { $src_file:
+        source => $url,
+        extract => true,
+        extract_path => dirname($src_dir),
+        creates => $src_dir,
+        cleanup => true,
+        user => 'root',
+        group => 'root',
+        notify => Exec["autoconf-${title}"]
+    }
+
+    # Build by configuring followed by make.
+    exec { "autoconf-${title}":
+        path => '/usr/bin:/bin:/usr/sbin:/sbin',
+        cwd => $src_dir,
+        command => 'autoconf',
+        refreshonly => true
+    }
+    ~> exec { "configure-${title}":
+        path => "${src_dir}:/usr/bin:/bin:/usr/sbin:/sbin",
+        cwd => $src_dir,
+        command => "./configure ${configure_flags}",
+        refreshonly => true
+    }
+    ~> exec { "make-${title}":
+        path => '/usr/bin:/bin:/usr/sbin:/sbin',
+        cwd => $src_dir,
+        command => "make ${make_target}",
+        refreshonly => true
+    }
+    ~> exec { 'ldconfig-${title}':
+        path => '/usr/bin:/bin:/usr/sbin:/sbin',
+        command => 'ldconfig',
+        refreshonly => true
+    }
+
+}
---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# @summary Installs Python from source.
+#
+# This module downloads Python sources from python.org and builds and installs
+# the Python interpreter from there.
+#
+# @param source_dir The directory where to download and extract the sources to.
+# @param base_url The base URL where the Python sources are downloaded from.
+#                 This defaults to the FTP directory on python.org.
+# @param build_dependencies A list of packages that need to be installed in
+#                           order to successfully build Python.
+# @param versions The Python versions to be downloaded and installed. The keys
+#                 of the hash are the version numbers. The values are a hash
+#                 holding additional parameters passed to the build process.
+#                 At the moment, only "override_url" is supported, which forces
+#                 the download from the specified URL rather than constructing
+#                 the URL from the base_url and the version string.
+#
+# @author Christoph Müller
+class pythonbuild(
+        String $source_dir = '/usr/local/src',
+        #String $install_dir = '/usr/local/bin',
+        String $base_url = 'https://www.python.org/ftp/python',
+        Array[String] $build_dependencies,
+        Hash[String, Hash[String, Variant[String, Integer, Boolean]]] $versions = { }
+        ) {
+
+    # Ensure that all build depedencies are installed.
+    ensure_packages($build_dependencies)
+
+    # Build all of the requested versions
+    $versions.each | $version, $attributes | {
+        pythonbuild::build { "pythonbuild-${version}":
+            source_dir => $source_dir,
+            #install_dir => $install_dir,
+            base_url => $base_url,
+            version => $version,
+            * => $attributes
+        }
+    }
+}
